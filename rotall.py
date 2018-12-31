4892a9158619a37aedcdbb684c31f6ef71f63bbd
drw <drw@users.noreply.github.com>
2018-12-31 16:26:21 -0500

    Add chi-squared test statistic to help identify best Caesar shift

--- rotall.py
+++ rotall.py
@@ -1,32 +1,91 @@
 import sys
 from pprint import pprint
 
+english_letter_freqs = {
+    "a": 8.167,
+    "b": 1.492,
+    "c": 2.782,
+    "d": 4.253,
+    "e": 12.702,
+    "f": 2.228,
+    "g": 2.015,
+    "h": 6.094,
+    "i": 6.966,
+    "j": 0.153,
+    "k": 0.772,
+    "l": 4.025,
+    "m": 2.406,
+    "n": 6.749,
+    "o": 7.507,
+    "p": 1.929,
+    "q": 0.095,
+    "r": 5.987,
+    "s": 6.327,
+    "t": 9.056,
+    "u": 2.758,
+    "v": 0.978,
+    "w": 2.360,
+    "x": 0.150,
+    "y": 1.974,
+    "z": 0.074
+} # Taken from here: https://gist.github.com/evilpacket/5973230
+
 try:
     input = raw_input # Ensure this script works under Python 2 as well as Python 3.
 except NameError:
     pass
 
-def encrypt(string, shift):
+def chisquared_stat(observed_values,expected_values):
+    # The original version of this function (which just calculates the chi-squared test
+    # statistic without relying on SciPy) can be found here:
+    # http://specminor.org/2017/01/08/performing-chi-squared-gof-python.html
+    test_statistic = 0
+
+    for observed, expected in zip(observed_values, expected_values):
+        test_statistic += (float(observed)-float(expected))**2/float(expected)
+
+    df = len(observed_values)-1
+
+    return test_statistic
+
+def encrypt_and_count(string, shift):
     # This is a variant on the Caesar cipher shift published at
     # https://www.thecrazyprogrammer.com/2018/05/caesar-cipher-in-python.html
+    # extended to analyze letter frequencies.
     cipher = ''
+
+    counts = [0] * 26
     for char in string:
         if char == ' ':
             cipher += char
         elif char.isupper():
-            cipher += chr((ord(char) + shift - 65) % 26 + 65)
+            n = (ord(char) + shift - 65) % 26
+            cipher += chr(n + 65)
+            counts[n] += 1
         elif char.islower():
-            cipher += chr((ord(char) + shift - 97) % 26 + 97)
+            n = (ord(char) + shift - 97) % 26
+            cipher += chr(n + 97)
+            couns[n] += 1
         else:
             cipher += char
 
-    return cipher
+    return cipher, counts
 
 if len(sys.argv) == 1:
     text = input("Enter string to shift: ")
-else: 
+else:
     text = ' '.join([str(x) for x in sys.argv[1:]])
 
+expected_unnormalized = english_letter_freqs.values()
+shifted, observed = encrypt_and_count(text,0)
+expected = [e*sum(observed)/sum(expected_unnormalized) for e in expected_unnormalized]
+
 print("Shifting {} by all possible shifts.".format(text))
+chisquared_min = 9999999999999
+shifted_min = None
 for shift in range(0,26):
-    print("{:>2}  |  {}".format(shift,encrypt(text,shift)))
+    shifted, observed = encrypt_and_count(text,shift)
+    chisquared = chisquared_stat(observed,expected)
+    print("{:>2}  |  {}  | {:>8.1f}".format(shift,shifted,chisquared))
+
+
